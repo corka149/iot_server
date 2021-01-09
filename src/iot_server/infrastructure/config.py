89af01f114b9e1bcfc33e380f3e818e3202d1901
corka149 <corka149@mailbox.org>
2021-01-09 13:39:39 +0100

    Reworked configuration

---
+++ src/iot_server/infrastructure/config.py
@@ -0,0 +1,72 @@
+"""
+Config module that tries to fetch the matching config for given profile
+ from PROJECT_PATH/config.
+ """
+import logging
+import os
+from pathlib import Path
+from typing import Dict, Any
+
+import yaml
+
+_config_yaml: Dict[str, Any]
+_log = logging.getLogger(__name__)
+
+
+def init(profile: str):
+    """ Init config file from path. """
+    global _config_yaml
+
+    config_path = os.path.abspath(os.path.dirname(__file__))
+    config_path = os.path.join(config_path, f'../../../config/iot_server-{profile.lower().strip()}.yaml')
+    yaml_path = Path(config_path)
+    if yaml_path.exists():
+        with yaml_path.open('r') as yaml_file:
+            _config_yaml = yaml.load(yaml_file, Loader=yaml.Loader)
+    else:
+        _config_yaml = dict()
+        _log.debug('No "%s" found.', config_path)
+
+
+def get_config(name: str):
+    """
+    Checks first the environment variable for a config.
+    When it is not available it will look for it in the
+    config file. It can only fetch simple types (int,
+    str, float) or a list of str from an environment variable.
+    The list must be colon separated.
+
+    Example for config name: "database.host" is equal to
+
+    in config file
+    '''
+    database:
+        host: localhost
+    '''
+
+    OR
+
+    environment file
+    '''
+    DATABASE_HOST
+    '''
+
+    """
+    # 1. Check environment variables
+    env_name = name.replace('.', '_').upper()
+    env_val = os.getenv(env_name)
+    if env_val:
+        if ';' in env_val:
+            return [v.strip() for v in env_val.split(';')]
+        return env_val
+
+    # 2. Check config file
+    keys = name.split('.')
+    val = _config_yaml
+    for k in keys:
+        if isinstance(val, Dict):
+            val = val.get(k, {})
+
+    if val:
+        return val
+    raise ValueError(f'"{name} not found')
--- src/iot_server/main.py
+++ src/iot_server/main.py
@@ -4,16 +4,14 @@ import mongoengine
 import uvicorn
 from fastapi import FastAPI
 
+from iot_server.infrastructure import config
+
 api = FastAPI()
 
 
 def configure_database():
-    db_host = os.getenv('DB_HOST', '127.0.0.1')
-    db_port = os.getenv('DB_PORT', 27017)
-    db_username = os.getenv('DB_USERNAME', 'admin')
-    db_password = os.getenv('DB_PASSWORD', 's3cr3t')
-    mongoengine.connect('iot', host=db_host, port=db_port, username=db_username, password=db_password,
-                        authentication_source='admin')
+    db_conf = config.get_config('database')
+    mongoengine.connect('iot', **db_conf)
 
 
 def configure_routes():
@@ -23,6 +21,7 @@ def configure_routes():
 
 
 def configure():
+    config.init(os.getenv('IOT_SERVER_PROFILE', 'dev'))
     configure_database()
     configure_routes()
 
