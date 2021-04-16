d764480eb26dc4310f96c96661682647584f5db6
corka149 <corka149@mailbox.org>
2021-04-16 09:45:48 +0200

    Changed stats logging for exchange

--- src/iot_server/service/exchange_service.py
+++ src/iot_server/service/exchange_service.py
@@ -1,7 +1,7 @@
 """ Exchange message between devices """
 import logging
 from collections import defaultdict
-from typing import Dict, List, Tuple
+from typing import Dict
 
 from starlette.websockets import WebSocket
 
@@ -10,32 +10,31 @@ from iot_server.model.message import MessageDTO, MessageType
 
 class ExchangeService:
     """ Tiny websocket broadcaster that store in memory websocket connections. """
-    _connections: Dict[str, List[Tuple[str, WebSocket]]] = defaultdict(list)
+    _connections: Dict[str, Dict[str, WebSocket]] = defaultdict(dict)
     _log = logging.getLogger('ExchangeService')
 
     @classmethod
     def register(cls, device_name: str, access_id: str, websocket: WebSocket):
         """ Registers a new websocket connection. """
-        cls._connections[device_name].append((access_id, websocket))
-        cls._log.info('%d registered access ids for device %s', len(cls._connections[device_name]), device_name)
+        cls._log.info('Register id %s for %s', access_id, device_name)
+        cls._connections[device_name][access_id] = websocket
+        cls._log_stats()
 
     @classmethod
     def remove(cls, device_name: str, access_id: str):
         """ Removes a websocket connection from the connection store. """
-        cls._log.info('Removed access id "%s" for device "%s" (%d register devices)',
-                      access_id, device_name, len(cls._connections[device_name]))
-
-        new_sockets = [(a_id, ws) for a_id, ws
-                       in cls._connections[device_name] if a_id != access_id]
-        cls._connections[device_name] = new_sockets
+        cls._log.info('Remove id %s from %s', access_id, device_name)
+        del cls._connections[device_name][access_id]
+        cls._log_stats()
 
     @classmethod
     async def dispatch(cls, device_name: str, sender_id: str, message: MessageDTO):
         """ Dispatches a message to 0, 1 or n targets """
         is_broadcast = message.target == MessageType.BROADCAST.value
 
-        cls._log.info('%d potential targets for message', len(cls._connections[device_name]))
-        for access_id, web_socket in cls._connections[device_name]:
+        cls._log_stats()
+
+        for access_id, web_socket in cls._connections[device_name].items():
             web_socket: WebSocket = web_socket
             if is_broadcast and access_id != sender_id:
                 cls._log.info('Send message to access id "%s"', access_id)
@@ -45,3 +44,10 @@ class ExchangeService:
                 cls._log.info('Send message to access id "%s"', access_id)
                 await web_socket.send_text(message.json())
                 return
+
+    @classmethod
+    def _log_stats(cls):
+        cls._log.info('Connections hold by "%d"', id(cls._connections))
+        for key in cls._connections:
+            ids = cls._connections[key].keys()
+            cls._log.info('Registered ids for %s: %s', key, ', '.join(ids))
