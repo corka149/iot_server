bab73fa63ce2498d52cd41c5121fec7182ce79e1
corka149 <corka149@mailbox.org>
2021-05-02 10:24:27 +0200

    Fixed payload transmission

--- src/iot_server/core/exchange_service.py
+++ src/iot_server/core/exchange_service.py
@@ -18,7 +18,7 @@ class PoolBoy:
     """Takes care about Redis connection pool"""
 
     _instance = None
-    __pool: Redis
+    _pool: Redis
 
     def __new__(cls, host: str, port: int, db: str, password: str):
         if not cls._instance:
@@ -34,15 +34,15 @@ class PoolBoy:
 
     @property
     async def pool(self) -> Redis:
-        if not self.__pool:
-            self.__pool = await aioredis.create_redis_pool(
+        if not self._pool:
+            self._pool = await aioredis.create_redis_pool(
                 f"redis://{self._host}:{self._port}",
                 encoding="utf8",
                 password=self._password,
                 db=self._db,
             )
 
-        return self.__pool
+        return self._pool
 
 
 class ExchangeService:
@@ -50,6 +50,7 @@ class ExchangeService:
 
     _log = logging.getLogger(__name__)
     _instance = None
+    _task_launched: bool
 
     def __new__(cls, pool_boy: PoolBoy):
         if not cls._instance:
@@ -57,10 +58,7 @@ class ExchangeService:
 
             cls._instance._connections = defaultdict(dict)
             cls._instance._boy = pool_boy
-
-            # Schedule listen task once
-            cls._log.info("Registered listen-task")
-            asyncio.create_task(cls._instance.listen())
+            cls._task_launched = False
 
         return cls._instance
 
@@ -70,6 +68,12 @@ class ExchangeService:
         self._log.info('Register id %s for "%s"', access_id, device_name)
         self._connections[device_name][access_id] = websocket
         self._log_stats()
+
+        # Listening makes only sense when someone registered
+        if not self._task_launched:
+            self._log.info("Registered listen-task")
+            asyncio.get_event_loop().create_task(self._instance.listen())
+            self._task_launched = True
 
     def remove(self, device_name: str, access_id: str):
         """Removes a websocket connection from the connection store."""
@@ -81,9 +85,10 @@ class ExchangeService:
         """Dispatches a message."""
         # Inform other workers
         pool = await self._boy.pool
+
         payload = {
+            **message.dict(),
             "device_name": device_name,
-            "message": message.dict(),
             "sender_id": sender_id,
         }
 
@@ -93,15 +98,15 @@ class ExchangeService:
         """Listens endless for messages from io message stream and distribute them."""
         pool = await self._boy.pool
         while True:
-            payloads = pool.xread([STREAM], latest_ids=LAST_ID, timeout=0, count=1)
+            payloads = pool.xread([STREAM], latest_ids=[LAST_ID], timeout=0, count=1)
             deliveries = list()
 
-            for payload in payloads:
+            for payload in await payloads:
                 self._log.info("Received %r", payload)
 
                 device_name = payload.get("device_name")
-                message = payload.get("message")
                 sender_id = payload.get("sender_id")
+                message = MessageDTO(**payload)
 
                 is_broadcast = message.target == MessageType.BROADCAST.value
 
